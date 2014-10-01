b863584af7701e144b0f947cf8bb7f0c96c416f4
Conrado Miranda <miranda.conrado@gmail.com>
2014-10-01 09:59:44 -0300

    Changed order of tests.

--- test/mpi_handler.cpp
+++ test/mpi_handler.cpp
@@ -4,26 +4,6 @@
 
 #include "test_mpi.hpp"
 
-TEST(MPIHandler, InsertGenericHandler) {
-  int received_tag = 0;
-
-  MPIHandler handler(world);
-  handler.insert_generic(
-      [&](int source, int tag) { received_tag = tag; return false; });
-  world.barrier();
-
-  if (world.rank() == 1) {
-    int val = 3;
-    world.send(0, 2, val);
-  }
-  world.barrier();
-
-  handler.run();
-  if (world.rank() == 0)
-    EXPECT_EQ(2, received_tag);
-  world.barrier();
-}
-
 TEST(MPIHandler, ClearGenericHandler) {
   int received_tag = 0;
 
@@ -42,26 +22,6 @@ TEST(MPIHandler, ClearGenericHandler) {
   handler.run();
   if (world.rank() == 0)
     EXPECT_EQ(0, received_tag);
-  world.barrier();
-}
-
-TEST(MPIHandler, InsertHandler) {
-  bool received_specific = false;
-
-  MPIHandler handler(world);
-  handler.insert(2,
-      [&](int source) { received_specific = true; return false; });
-  world.barrier();
-
-  if (world.rank() == 1) {
-    int val = 3;
-    world.send(0, 2, val);
-  }
-  world.barrier();
-
-  handler.run();
-  if (world.rank() == 0)
-    EXPECT_TRUE(received_specific);
   world.barrier();
 }
 
@@ -85,3 +45,44 @@ TEST(MPIHandler, ClearHandler) {
     EXPECT_FALSE(received_specific);
   world.barrier();
 }
+
+TEST(MPIHandler, InsertGenericHandler) {
+  int received_tag = 0;
+
+  MPIHandler handler(world);
+  handler.insert_generic(
+      [&](int source, int tag) { received_tag = tag; return false; });
+  world.barrier();
+
+  if (world.rank() == 1) {
+    int val = 3;
+    world.send(0, 2, val);
+  }
+  world.barrier();
+
+  handler.run();
+  if (world.rank() == 0)
+    EXPECT_EQ(2, received_tag);
+  world.barrier();
+}
+
+TEST(MPIHandler, InsertHandler) {
+  bool received_specific = false;
+
+  MPIHandler handler(world);
+  handler.insert(2,
+      [&](int source) { received_specific = true; return false; });
+  world.barrier();
+
+  if (world.rank() == 1) {
+    int val = 3;
+    world.send(0, 2, val);
+  }
+  world.barrier();
+
+  handler.run();
+  if (world.rank() == 0)
+    EXPECT_TRUE(received_specific);
+  world.barrier();
+}
+
